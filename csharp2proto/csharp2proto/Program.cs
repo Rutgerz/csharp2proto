6801ee96532935179ea435788d48efec4a20b7cc
Rutgerz <r.schaafsma@gmail.com>
2017-02-28 23:44:33 +0100

    Reflect over public, non-static, props and field

---
+++ csharp2proto/csharp2proto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp2proto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            var aType = obj.GetType();
+            foreach(var propType in aType.GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public).Where(x => x.CanRead))
+            {
+                Console.WriteLine($"{aType.Name}.{propType.Name} of type {propType.PropertyType}");
+            }
+            foreach (var fieldType in aType.GetFields().Where(x => !x.IsStatic))
+            {
+                Console.WriteLine($"{aType.Name}.{fieldType.Name} of type {fieldType.FieldType}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public class A
+        {
+            public A self;
+            public string Str { get; set; }
+            public int Int { get; set; }
+            public IList<double> DoubleList { get; set; }
+            public IDictionary<int, IList<string>> DictIntStringList { get; set; }
+
+            public static bool IsEvil { get; set; }
+        }
+
+        public class B : A
+        {
+            public C fruit;
+            public float Float { get; set; }
+        }
+
+        public struct C
+        {
+            bool apples;
+            bool bananas;
+        }
+    }
+}
